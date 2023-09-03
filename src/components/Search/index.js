f18d12342e3bb9e588bc99c1de5e2138d0647bb2
Gustavo <gustavo.hpaula@outlook.com>
2023-09-03 20:03:21 -0300

    finalizando useState pesquisa de livros listados pelo app

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,7 @@
 import Input from "../Input";
 import styled from "styled-components";
 import { useState } from "react";
+import { livros } from "./searchData";
 
 const SearchContainer = styled.section`
     background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
@@ -13,6 +14,7 @@ const SearchContainer = styled.section`
     padding: 120px 0px;
     height: 270px;
     width: 100%;
+
 `
 
 const Titulo = styled.h2`
@@ -28,10 +30,31 @@ const SubTitulo = styled.h3`
     margin-bottom: 40px;
 `
 
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    width: 700px;
+
+    p {
+        width: 200px;
+    }
+
+    img {
+        width: 100px;
+    }
+
+    &:hover {
+        border: 1px solid #fff;
+    }
+`
+
 function Search() {
     const [
-        TextoDigitado, setTextoDigitado
-    ] = useState('')
+        livrosPesquisados, setLivrosPesquisados
+    ] = useState([])
 
     return (
         <SearchContainer>
@@ -39,9 +62,18 @@ function Search() {
             <SubTitulo>Encontre seu livro em nossa estante</SubTitulo>
             <Input 
                 placeholder="Escreva sua proxima leitura"
-                onBlur={e => setTextoDigitado(e.target.value)}
+                onBlur={ e => {
+                    const TextoDigitado = e.target.value
+                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(TextoDigitado) )
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
             ></Input>
-            <p>{TextoDigitado}</p>
+            {livrosPesquisados.map( livro => (
+                <Resultado>
+                    <p>{livro.nome}</p>
+                    <img src={livro.src} />
+                </Resultado>
+            ))}
         </SearchContainer>
     );
 }
---
+++ src/components/Search/searchData.js
@@ -0,0 +1,9 @@
+import livro from '../../img/livro.png'
+
+export const livros = [
+    { nome: 'Liderança em Design', id: 1, src: livro },
+    { nome: 'Javascript assertivo', id: 2, src: livro },
+    { nome: 'Lorem ipsum', id: 3, src: livro },
+    { nome: 'Aventureiros em Java', id: 4, src: livro },
+    { nome: 'Desenvolvimento em Banco de Dados', id: 5, src: livro }
+]
