576e0cda5e76036d274431f86e20ed112ce0a202
Gustavo <gustavo.hpaula@outlook.com>
2023-09-02 23:18:54 -0300

    conteinerizacao

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,22 @@
-import "./style.css";
 import Logo from "../Logo";
 import NavHeader from "../NavHeader";
 import IconesHeader from "../IconsHeader";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-around;
+  background-color: hsl(0, 0%, 100%);
+  padding: 0 12px;
+`
 
 function Header() {
   return (
-    <header className="App-header">
+    <HeaderContainer>
       <Logo />
       <NavHeader />
       <IconesHeader />
-    </header>
+    </HeaderContainer>
   );
 }
 
